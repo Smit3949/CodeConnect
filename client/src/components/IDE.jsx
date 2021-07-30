80944b794d2c6bc0c011b73d6fcf2bb1bc62a8e4
vivek9patel <vivek.p9737@gmail.com>
2021-07-30 17:03:20 +0530

    fix: final design

--- client/src/components/IDE.jsx
+++ client/src/components/IDE.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { io, Socket } from 'socket.io-client';
+import { io } from 'socket.io-client';
 import { Controlled as CodeMirror } from 'react-codemirror2';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/material.css';
@@ -9,24 +9,21 @@ import 'codemirror/mode/javascript/javascript';
 import 'codemirror/mode/clike/clike';
 import 'codemirror/mode/python/python';
 import { useParams } from 'react-router';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import Peer from 'peerjs';
-import VideoTile from './VideoTile';
-import ResizablePanels from './Resizable';
-import { ReactSketchCanvas } from "react-sketch-canvas";
-import logo from '../images/logo.svg';
-import upArrow from '../images/icons/up-arrow.svg';
 import runIcon from '../images/icons/run.svg';
 import closeIcon from '../images/icons/close.png';
 import muteIcon from '../images/icons/mute.svg';
 import videoIcon from '../images/icons/video.svg';
 import phoneIcon from '../images/icons/phone.svg';
-import { Icon, InlineIcon } from '@iconify/react';
+import { Icon } from '@iconify/react';
 import eraser24Filled from '@iconify/icons-fluent/eraser-24-filled';
 import penFill from '@iconify/icons-bi/pen-fill';
 import whiteboard24Regular from '@iconify/icons-fluent/whiteboard-24-regular';
+import 'react-circular-progressbar/dist/styles.css';
 
 
-export default function IDE({ }) {
+export default function IDE() {
   const { id: DocId } = useParams();
   const [socket, setSocket] = useState(null);
   const [cpp, setcpp] = useState('');
@@ -37,18 +34,18 @@ export default function IDE({ }) {
   const [input, setInput] = useState('');
   const [output, setOutput] = useState('');
   const [modal, setModal] = useState(false);
-  const [username, setUsername] = useState('smit');
-  const outputRef = useRef(null);
+  const username = 'smit'
   const videoGrid = document.getElementById('video-grid');
   const myVideo = document.createElement('video');
   myVideo.className = "rounded mb-4"
   myVideo.muted = true;
   const [myStream, setMystream] = useState(null);
   const peers = {};
-  const canvasRef = useRef(null);
   const colorsRef = useRef(null);
   const [userId, setUserId] = useState(null);
   const [textEditor, setTextEditor] = useState('input');
+  const [processing, setProcessing] = useState(false);
+  const [percentageStage, setPercentageStage] = useState(0);
 
 
 
@@ -148,7 +145,7 @@ export default function IDE({ }) {
         call.on('stream', (anotherUserVideoStream) => {
 
           console.log(anotherUserVideoStream.getAudioTracks());
-          addVideoStream(video, anotherUserVideoStream);
+          addVideoStream(video, anotherUserVideoStream, username);
         });
 
         call.on('close', () => {
@@ -168,7 +165,7 @@ export default function IDE({ }) {
       setUserId(id);
       socket.emit('join-room', DocId, id, username);
     });
-
+    // eslint-disable-next-line
   }, [socket, DocId, peer]);
 
   const muteMic = () => {
@@ -196,19 +193,25 @@ export default function IDE({ }) {
 
     if (socket === null || colorsRef === null) return;
     const canvas = document.getElementById('whiteboard-canvas')
-    const test = colorsRef.current;
     const context = canvas.getContext('2d');
 
     const colors = document.getElementsByClassName('color');
-    console.log(colors, 'the colors');
-    console.log(test);
+    // console.log(colors, 'the colors');
+    // console.log(test);
     const current = {
       color: 'black',
       width: 5,
     };
 
     const onColorUpdate = (e) => {
-      current.color = e.target.className.split(' ')[1];
+      let objectColor;
+      for (let i = 0; i < e.path.length; i++) {
+        if (e.path[i].dataset.color) {
+          objectColor = e.path[i].dataset.color;
+          break;
+        }
+      }
+      current.color = objectColor;
       if (current.color === 'black') current.width = 5;
       else current.width = 25;
     };
@@ -231,7 +234,7 @@ export default function IDE({ }) {
       if (!emit) { return; }
       const w = canvas.width;
       const h = canvas.height;
-      console.log(w, h, window.width, window.height);
+      // console.log(w, h, window.width, window.height);
 
 
       socket.emit('drawing', {
@@ -248,7 +251,7 @@ export default function IDE({ }) {
 
     const onMouseDown = (e) => {
 
-      console.log(drawing + ' d');
+      // console.log(drawing + ' d');
       drawing = true;
       current.x = e.clientX || e.touches[0].clientX;
       current.y = e.clientY || e.touches[0].clientY;
@@ -306,11 +309,16 @@ export default function IDE({ }) {
     socket.on('drawing', onDrawingEvent);
   }, [socket]);
 
-
+  let statusLoop = null;
 
   const runCode = () => {
+    setOutput('')
+    setTextEditor('output');
+    setProcessing(true);
+    setPercentageStage(10);
+
     var lang = selected;
-    console.log(lang, input);
+    // console.log(lang, input);
     var backend_url = 'https://api.hackerearth.com/v4/partner/code-evaluation/submissions/';
 
     var data = {
@@ -332,7 +340,7 @@ export default function IDE({ }) {
       credentials: 'same-origin',
       headers: {
         'Content-Type': 'application/json',
-        'client-secret': '0b281b4a748997e754b1c8afd4dbfb64fb2835ea'
+        'client-secret': process.env.REACT_APP_HACKEREARTH_SECRET
       },
       redirect: 'follow',
       referrerPolicy: 'no-referrer',
@@ -341,52 +349,75 @@ export default function IDE({ }) {
       .then((res) => res.json())
       .then((data) => {
         status = data.status_update_url;
-        console.log(data);
 
-        fetch(status, {
-          method: 'GET',
-          mode: 'cors',
-          cache: 'no-cache',
-          credentials: 'same-origin',
-          headers: {
-            'Content-Type': 'application/json',
-            'client-secret': '0b281b4a748997e754b1c8afd4dbfb64fb2835ea'
-          },
-          redirect: 'follow',
-          referrerPolicy: 'no-referrer',
-        })
-          .then((res) => res.json())
-          .then((data) => {
-            console.log(data);
-            fetch(status, {
-              method: 'GET',
-              mode: 'cors',
-              cache: 'no-cache',
-              credentials: 'same-origin',
-              headers: {
-                'Content-Type': 'application/json',
-                'client-secret': '0b281b4a748997e754b1c8afd4dbfb64fb2835ea'
-              },
-              redirect: 'follow',
-              referrerPolicy: 'no-referrer',
-            })
-              .then((res) => res.json())
-              .then((data) => {
-                console.log(data.result.run_status.output);
-                var goToOutput = data.result.run_status.output;
+        setPercentageStage(25)
 
-              })
-              .catch(e => console.log(e));
+        statusLoop = setInterval(() => {
+          fetch(status, {
+            method: 'GET',
+            mode: 'cors',
+            cache: 'no-cache',
+            credentials: 'same-origin',
+            headers: {
+              'Content-Type': 'application/json',
+              'client-secret': process.env.REACT_APP_HACKEREARTH_SECRET
+            },
+            redirect: 'follow',
+            referrerPolicy: 'no-referrer',
           })
-          .catch(e => console.log(e));
-      })
-      .catch(e => console.log(e));
+            .then((res) => res.json())
+            .then((data) => {
+              setPercentageStage(75)
+              // console.log(data);
+              if (data.result.compile_status === 'OK') {
+                if (data.result.run_status.status === 'AC') {
+                  getOutput(data.result.run_status.output);
+                  clearInterval(statusLoop);
+                }
+                else if (data.result.run_status.status === 'OLE') {
+                  setOutput(data.result.run_status.status_detail);
+                  setProcessing(false);
+                  clearInterval(statusLoop);
+                }
+                else if (data.result.run_status.status !== 'NA') {
+                  setOutput(data.result.run_status.stderr);
+                  setProcessing(false);
+                  clearInterval(statusLoop);
+                }
+              }
+              else {
+                setOutput(data.result.compile_status);
+                setProcessing(false);
+                clearInterval(statusLoop);
+                return;
+              }
+            })
+            .catch(e => {
+              setProcessing(false);
+              clearInterval(statusLoop);
+              console.log(e)
+            });
+        }, 2000)
+      }).catch(e => {
+        console.log(e)
+      });;
+
   };
 
-  const styles = {
-    border: "0.0625rem solid #9c9c9c",
-    borderRadius: "0.25rem"
-  };
+  const getOutput = async (link) => {
+    await fetch(link).then((data) => {
+      data.json().then((res) => {
+        setPercentageStage(100)
+        setProcessing(false);
+        setOutput(res);
+      }).catch(e => {
+        setProcessing(false);
+      })
+    })
+      .catch(e => {
+        setProcessing(false);
+      })
+  }
 
   const toggleModal = () => {
     setModal(!modal);
@@ -419,10 +450,13 @@ export default function IDE({ }) {
                 {
                   selected === 'CPP' &&
                   <section className="playground">
-                    <div className="code-editor-cpp cpp-code h-full">
-                      <div className="editor-header">CPP</div>
+                    <div className="code-editor-java flex flex-col h-full mb-5 java-code">
+                      <div className="editor-header">
+                        <LanguageSelector language={selected.toLowerCase()} setLanguage={setSelected} />
+                      </div>
                       <CodeMirror
                         value={cpp}
+                        className="flex-grow text-base"
                         options={{
                           mode: "text/x-csrc",
                           theme: 'material',
@@ -440,10 +474,13 @@ export default function IDE({ }) {
                 {
                   selected === 'JAVA' &&
                   <section className="playground">
-                    <div className="code-editor-java java-code h-full">
-                      <div className="editor-header">java</div>
+                    <div className="code-editor-java flex flex-col h-full mb-5 java-code">
+                      <div className="editor-header">
+                        <LanguageSelector language={selected.toLowerCase()} setLanguage={setSelected} />
+                      </div>
                       <CodeMirror
                         value={java}
+                        className="flex-grow text-base"
                         options={{
                           mode: "text/x-java",
                           theme: 'material',
@@ -462,10 +499,12 @@ export default function IDE({ }) {
                   selected === 'PYTHON' &&
                   <section className="playground">
                     <div className="code-editor-java flex flex-col h-full mb-5 java-code">
-                      <div className="editor-header">python</div>
+                      <div className="editor-header">
+                        <LanguageSelector language={selected.toLowerCase()} setLanguage={setSelected} />
+                      </div>
                       <CodeMirror
                         value={python}
-                        className="flex-grow"
+                        className="flex-grow text-base"
                         options={{
                           mode: "python",
                           theme: 'material',
@@ -482,9 +521,9 @@ export default function IDE({ }) {
                 }
               </div>
               <div className={`flex-grow ${modal ? "top-0" : " top-full"} duration-300 left-0 p-4 backdrop-filter backdrop-blur-sm absolute z-50 w-screen h-screen`}>
-                <div ref={colorsRef} className="colors absolute select-none left-10 top-10">
-                  <button  ><Icon icon={penFill} className="color black text-orange-standard" height="28" /></button>
-                  <button className="ml-4"><Icon icon={eraser24Filled} className="color white" height="30" /></button>
+                <div ref={colorsRef} className="colors absolute flex select-none left-10 top-10">
+                  <Icon icon={penFill} data-color="black" className="block cursor-pointer color black text-orange-standard" height="28" />
+                  <Icon icon={eraser24Filled} data-color="white" className="block cursor-pointer color white ml-4" height="30" />
                 </div>
                 <div className="absolute right-10 select-none top-10">
                   <img onClick={toggleModal} src={closeIcon} className="w-6 cursor-pointer" alt="close icon" />
@@ -504,13 +543,26 @@ export default function IDE({ }) {
                     }}
                     className={`cursor-pointer w-1/2 ${(textEditor === 'output' ? "hover:opacity-90 border-black" : " hover:opacity-60 border-transparent opacity-50")} border-l bg-orange-standard`}>Output</div>
                 </div>
-                <div className="w-full flex-grow p-4">
+                <div className="w-full flex-grow p-4 relative">
                   {
                     textEditor === 'input' ?
                       <textarea className="rounded-md outline-none shadow-md w-full h-full p-4 resize-none" placeholder="enter an input..." onChange={(e) => { setInput(e.target.value) }} value={input} rows="4" cols="50">
                       </textarea>
-                      : <textarea className="rounded-md outline-none shadow-md w-full h-full p-4 resize-none" readOnly placeholder="output will be shown here" value={output} rows="4" cols="50">
+                      : <textarea className={` ${processing ? "transform animate-pulse" : ""} rounded-md outline-none shadow-md w-full h-full p-4 resize-none`} readOnly placeholder="output will be shown here" value={output} rows="4" cols="50">
                       </textarea>
+                  }
+                  {
+                    processing &&
+                    <div className="absolute z-20 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ">
+                      <CircularProgressbar styles={buildStyles({
+                        rotation: 0.25,
+                        strokeLinecap: 'butt',
+                        textSize: '22px',
+                        pathTransitionDuration: 0.5,
+                        pathColor: `#EE9B00`,
+                        textColor: '#EE9B00'
+                      })} className="h-20" value={percentageStage} text={`${percentageStage}%`} />
+                    </div>
                   }
                 </div>
                 <input accept="text/plain" type="file" onChange={handleFileDataChange} className="hidden" id="input-file-upload" />
@@ -528,10 +580,12 @@ export default function IDE({ }) {
 
 function Header({ runCode, toggleModal }) {
   return (
-    <div className=" bg-purple-standard flex py-2 px-2 justify-end items-center">
-      {/* <div className="h-16">
-        <img className="h-full" src={logo} alt="codeconnect logo" />
-      </div> */}
+    <div className=" bg-purple-standard flex py-2 px-2 justify-between items-center">
+      <div className="flex items-center">
+        <div className="h-7">
+          <img className="h-full" src={'./logo.png'} alt="codeconnect logo" />
+        </div>
+      </div>
       <div className="flex">
         <button className=" text-white mr-4" onClick={toggleModal}><Icon icon={whiteboard24Regular} className="text-orange-standard" height="28" /></button>
         <button onClick={runCode} className="bg-orange-standard flex items-center text-base font-medium rounded px-3 py-1 mr-2">
@@ -569,6 +623,18 @@ function RightVideoPanel({ muteCam, muteMic }) {
   )
 }
 
+function LanguageSelector({ language, setLanguage }) {
+  return (
+    <select className="text-white cursor-pointer bg-transparent" onChange={(e) => {
+      setLanguage(e.target.value.toUpperCase())
+    }} value={language} name="language-selector">
+      <option value="python">python</option>
+      <option value="cpp">cpp</option>
+      <option value="java">java</option>
+    </select>
+  )
+}
+
 function FileTabs({ files }) {
   return (
     <div className="w-full">
@@ -593,54 +659,53 @@ function FileTabs({ files }) {
 }
 
 
-function SidePanel() {
-  return (
-    <div className="bg-purple-dark text-orange-standard w-20">
-      <span>Share Room ID</span>
-      <br />
-      <span>Join Room</span>
-      <br />
-      <span>Download</span>
-      <br />
-    </div>
-  )
-}
+// function SidePanel() {
+//   return (
+//     <div className="bg-purple-dark text-orange-standard w-20">
+//       <span>Share Room ID</span>
+//       <br />
+//       <span>Join Room</span>
+//       <br />
+//       <span>Download</span>
+//       <br />
+//     </div>
+//   )
+// }
 
-function ShareRoomID() {
-  const currentURL = window.location.href;
-  return (
-    <div className="bg-orange-standard text-purple-dark p-5">
-      Share
-      <div className="my-5 text-purple-dark">
-        <span className="bg-grey-standard w-min rounded-l px-3 py-1 align-middle">
-          {currentURL}
-        </span>
-        <span onClick={() => navigator.clipboard.writeText(window.location.href)} className="bg-grey-standard bg-opacity-50 w-min px-3 py-1 rounded-r align-middle">
-          Copy
-        </span>
-      </div>
-      <div className="text-purple-dark">
-        NOTE: Anyone with the link can join & edit the code
-      </div>
-    </div>
-  )
-}
+// function ShareRoomID() {
+//   const currentURL = window.location.href;
+//   return (
+//     <div className="bg-orange-standard text-purple-dark p-5">
+//       Share
+//       <div className="my-5 text-purple-dark">
+//         <span className="bg-grey-standard w-min rounded-l px-3 py-1 align-middle">
+//           {currentURL}
+//         </span>
+//         <span onClick={() => navigator.clipboard.writeText(window.location.href)} className="bg-grey-standard bg-opacity-50 w-min px-3 py-1 rounded-r align-middle">
+//           Copy
+//         </span>
+//       </div>
+//       <div className="text-purple-dark">
+//         NOTE: Anyone with the link can join & edit the code
+//       </div>
+//     </div>
+//   )
+// }
 
-// TODO
-function JoinRoom() {
-  const [input, setInput] = useState('');
-  return (
-    <div className="bg-orange-standard text-purple-dark p-5">
-      Join
-      <div className="my-5 text-purple-dark">
-        <input type="text" value={input} onInput={e => setInput(e.target.value)} className="bg-grey-standard w-min rounded-l px-3 py-1 align-middle outline-none border-none" />
-        <button className="bg-grey-standard bg-opacity-50 w-min px-3 py-1 rounded-r align-middle">
-          <a href={input}>Join</a>
-        </button>
-      </div>
-      <div className="text-purple-dark">
-        NOTE: Make sure you are entering correct URL
-      </div>
-    </div>
-  )
-}
+// function JoinRoom() {
+//   const [input, setInput] = useState('');
+//   return (
+//     <div className="bg-orange-standard text-purple-dark p-5">
+//       Join
+//       <div className="my-5 text-purple-dark">
+//         <input type="text" value={input} onInput={e => setInput(e.target.value)} className="bg-grey-standard w-min rounded-l px-3 py-1 align-middle outline-none border-none" />
+//         <button className="bg-grey-standard bg-opacity-50 w-min px-3 py-1 rounded-r align-middle">
+//           <a href={input}>Join</a>
+//         </button>
+//       </div>
+//       <div className="text-purple-dark">
+//         NOTE: Make sure you are entering correct URL
+//       </div>
+//     </div>
+//   )
+// }
